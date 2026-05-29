44e779fc8b79d3a7d3a15910f47d1c9cb974f74c
lamhuy <jason.huy.lam@gmail.com>
2026-05-28 22:36:59 -0400

    Fix unused imports in LoginPage and SignUpPage

--- webapp/src/pages/LoginPage.tsx
+++ webapp/src/pages/LoginPage.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { 
   IonPage, IonContent, IonHeader, IonToolbar, IonTitle, 
-  IonItem, IonLabel, IonInput, IonButton, IonText, IonLoading,
-  IonCard, IonCardContent
+  IonInput, IonButton, IonText, IonLoading
 } from '@ionic/react';
 import { loginWithUsername } from '../services/auth';
 import { useHistory, Link } from 'react-router-dom';
